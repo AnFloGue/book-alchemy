076d0c499a0158e38cb41cf5cd5656e9570b9eaf
AnFlGue <mokshaflores@icloud.com>
2024-08-28 20:36:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Book Alchemy API endpoints
+
+
+
+## Running the application
+```bash
+python -m flask run
+```
+
+### Home Page
+- URL: /
+- Method: GET
+- Description: Displays a list of all books.
+
+
+### Add Author Page
+- URL: /add_author
+- Method: GET, POST
+- Description: Displays a form to add a new author and handles form submission.
+
+### Add Book Page
+- URL: /add_book
+- Method: GET, POST
+- Description: Displays a form to add a new book and handles form submission.
+
+### Delete Book Page
+- URL: /book/<int:book_id>/delete
+- Method: POST
+- Description: Deletes a book by its ID.
---
+++ app.py
@@ -0,0 +1,55 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from data_models import db, Author, Book
+
+app = Flask(__name__)
+base_dir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(base_dir, "data", "library.sqlite")}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'supersecretkey'
+
+db.init_app(app)
+
+@app.route('/')
+def home():
+    books = Book.query.all()
+    return render_template('home.html', books=books)
+
+from datetime import datetime
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    if request.method == 'POST':
+        name = request.form['name']
+        birth_date = datetime.strptime(request.form['birth_date'], '%Y-%m-%d').date()
+        date_of_death = datetime.strptime(request.form['date_of_death'], '%Y-%m-%d').date()
+        new_author = Author(name=name, birth_date=birth_date, date_of_death=date_of_death)
+        db.session.add(new_author)
+        db.session.commit()
+        flash('Author added successfully!')
+        return redirect(url_for('add_author'))
+    return render_template('add_author.html')
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        isbn = request.form['isbn']
+        title = request.form['title']
+        publication_year = request.form['publication_year']
+        author_id = request.form['author_id']
+        new_book = Book(isbn=isbn, title=title, publication_year=publication_year, author_id=author_id)
+        db.session.add(new_book)
+        db.session.commit()
+        flash('Book added successfully!')
+        return redirect(url_for('add_book'))
+    authors = Author.query.all()
+    return render_template('add_book.html', authors=authors)
+
+@app.route('/book/<int:book_id>/delete', methods=['POST'])
+def delete_book(book_id):
+    book = Book.query.get(book_id)
+    if book:
+        db.session.delete(book)
+        db.session.commit()
+        flash('Book deleted successfully!')
+    return redirect(url_for('home'))
